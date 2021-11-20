e18cfaf151bc249e6c35a0fba1c2ee5c1b346349
Acxel Morales <acxelmorales97@gmail.com>
2021-11-19 21:48:02 -0600

    Toolbar and actions configuration added

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -3,9 +3,40 @@ package com.example.toolbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+import androidx.appcompat.widget.Toolbar
+
 class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_fav -> {
+                Toast.makeText(this, "Elemento añadido como favoritos", Toast.LENGTH_SHORT).show()
+                true
+            } else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
 }
